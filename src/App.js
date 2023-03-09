5f199fbdb726e726863677b28b25d0af20faa756
Ubuntu <ubuntu@ip-172-31-40-21.ap-south-1.compute.internal>
2023-03-09 08:49:20 +0000

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 
 import './App.css';
-import Dashboard from "./dashboard/Dashboard"
+// import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 
+import Dashboard from './dashboard/Dashboard';
+import Login from './Components/login/Login';
+import Staff from './Components/staff/Staff';
 function App() {
   return (
+    <>
     <Dashboard/>
+    {/* <Login/>
+    <Staff/> */}
+    </>
+    // <>
+    //   <Link to="/Staff">Staff</Link>
+    //   <Routes>
+    //     <Route path="./staff/Staff" element={<Staff />}></Route>
+    //   </Routes>
+    // </>
   );
 }
 
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Admin from '../Components/admin/Admin'
+import Staff from '../Components/staff/Staff'
+import Supervisor from '../Components/supervisor/Supervisor'
+import Login from '../Components/login/Login'
+import About from './About'
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Staff" element={<Login><Staff /></Login>} />
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/Supervisor" element={<Supervisor />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+
+    </div>
   )
 }
